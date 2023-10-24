a7af35a2445dc98bfd4286078a346ca703fc7794
an03er <144229434+an03er@users.noreply.github.com>
2023-10-24 19:11:56 +0300

    Update skript.js

--- skript.js
+++ skript.js
@@ -6,25 +6,30 @@ function calc() {
     if(count==""){alert("Вы не указали количество");}
     else if(count<0){alert("Количество не может быть отрицательным");}
     else{
-        var price = 1;
+        var price = 0;
         if (form.elements["toward"].value=="125"){
             document.getElementById("selection").style.display="none";
             document.getElementById("chec").style.display="none";
-            price = parseInt(count) * parseInt(form.elements["toward"].value);
             price += parseInt(form.elements["toward"].value);
+            price = parseInt(count) * price;
             document.getElementById('result').innerHTML = "Стоимость равна: "+ price +" р.";}
         else if(form.elements["toward"].value=="45"){
+            alert("HUI")
             document.getElementById("selection").style.display="block";
             document.getElementById("chec").style.display="none";
-            price = parseInt(count) * parseInt(form.elements["toward"].value);
-             price += parseInt(type_pie.options[type_pie.selectedIndex].value);
+            price += parseInt(form.elements["toward"].value);
+            price += parseInt(type_pie.options[type_pie.selectedIndex].value);
+            price = parseInt(count) * price;
             document.getElementById('result').innerHTML = "Стоимость равна: "+ price +" р.";
         }
         else if(form.elements["toward"].value=="100"){
             document.getElementById("selection").style.display="none";
             document.getElementById("chec").style.display="block";
-            price = parseInt(count) * parseInt(form.elements["toward"].value);
+            price += parseInt(form.elements["toward"].value);
+            price = parseInt(count) * price;
             if(chek.checked){price+=25;}
             document.getElementById('result').innerHTML = "Стоимость равна: "+ price +" р.";}
     }
 }
+
+window.addEventListener('click', calc);
